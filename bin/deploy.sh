1b77d1971fba2c8f12f1fa330cb085eb2a6430f2
Your Name <you@example.com>
2017-12-11 17:56:41 +0000

    ...

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -19,3 +19,6 @@ sudo apt-get install python
 sudo apt-get install python-pip
 sudo pip install docker-compose
 sudo chmod a+wrx /usr/local/bin/docker-compose
+sudo cp /home/qosit/bin/testcase.service /lib/systemd/system/testcase.service
+sudo systemctl enable testcase
+sudo systemctl start testcase
