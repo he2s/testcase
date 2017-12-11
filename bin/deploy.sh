9b1285697e12bf08854c79269847dc84d3452c0c
Your Name <you@example.com>
2017-12-11 17:47:47 +0000

    ...

---
+++ bin/deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo useradd qosit
+sudo passwd qosit
+sudo mkdir /home/qosit
+sudo chown qosit /home/qosit
+sudo usermod -aG ssh qosit
+sudo usermod -aG docker qosit
+sudo usermod -aG sudo qosit
+sudo systemctl enable ssh
+sudo systemctl enable docker
+sudo systemctl start ssh
+sudo systemctl start docker
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install tmux vlock wget curl nmap zsh fish htop sysstat
+sudo apt-get install docker.io
+sudo apt-get install python
+sudo apt-get install python-pip
+sudo pip install docker-compose
+sudo chmod a+wrx /usr/local/bin/docker-compose
